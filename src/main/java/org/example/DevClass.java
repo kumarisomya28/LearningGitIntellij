81615a29190e8bef5f1c9f8ef00e5e6db3a0b0dd
somya-11-java <kumarisomya073@gmail.com>
2024-07-20 12:04:46 +0530

    Update DevClass.java

--- src/main/java/org/example/DevClass.java
+++ src/main/java/org/example/DevClass.java
@@ -2,7 +2,9 @@ package org.example;
 
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("DevClass");
+        System.out.println("DevClass+somya-learn");
+        //checkin from somya-learn
+        
     }
 
 }
